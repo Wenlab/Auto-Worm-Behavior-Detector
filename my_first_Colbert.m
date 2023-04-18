be272e0e3ebf36adb2913e9b22b50bf40477445f
Physics-Lee <hyperdunk2019@mail.ustc.edu.cn>
2023-04-18 20:35:06 +0800

    2023-04-18

--- my_first_Colbert.m
+++ my_first_Colbert.m
@@ -1,14 +1,14 @@
-clc;clear;close all;
-folder = 'F:\1_learning\work\Colbert\NC';
-worm = 'NC20230312\w3';
-file_name = 'NC20230312w3_mcd';
-file_path = fullfile(folder,worm,file_name);
-load(file_path);
-frame_range = 33343:34000;
+% clc;clear;close all;
+% folder = 'F:\1_learning\work\Colbert\NC';
+% worm = 'NC20230312\w3';
+% file_name = 'NC20230312w3_mcd';
+% file_path = fullfile(folder,worm,file_name);
+% load(file_path);
+frame_range = 1:10000;
 center_of_centerline = zeros(length(frame_range),2);
 for i = frame_range
     centerline = NC20230312w3_mcd(i).SegmentedCenterline;
-    center_of_centerline(i-frame_range(1)+1,1:2) = calculate_the_center_of_a_centerline(centerline);
+    center_of_centerline(i-frame_range(1)+1,1:2) = calculate_the_center_of_a_centerline(centerline) + NC20230312w3_mcd(i).StagePosition;
 end
 
 % Extract x and y coordinates from the data array
