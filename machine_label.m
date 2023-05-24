31eb9e2a23a806ee5f5a947f37d0b1bcec6ad3b1
Physics-Lee <hyperdunk2019@mail.ustc.edu.cn>
2023-05-24 18:18:57 +0800

    process the neighbors of turns

--- machine_label.m
+++ machine_label.m
@@ -178,4 +178,26 @@ end
 label_rearranged_v3 = re_rearrange_label(label_rearranged_v3);
 label_rearranged = label_rearranged_v3;
 
+%% process neighbor of turns
+t_threshold_forward = 1.5; % s
+t_threshold_reversal = 1;
+for i = 2:length(label_rearranged)-1    
+    if label_rearranged(i,3) == 1
+        
+        % last neighbor should not be short forward
+        if label_rearranged(i-1,3) == 2 &&...
+                label_rearranged(i-1,2) - label_rearranged(i-1,1) <= t_threshold_forward*s2frame
+            label_rearranged(i-1,3) = 1;
+        end
+        
+        % next neighbor should not be short reversal
+        if label_rearranged(i+1,3) == 3 &&...
+                label_rearranged(i+1,2) - label_rearranged(i+1,1) <= t_threshold_reversal*s2frame
+            label_rearranged(i+1,3) = 1;
+        end
+        
+    end    
+end
+label_rearranged = re_rearrange_label(label_rearranged);
+
 end
