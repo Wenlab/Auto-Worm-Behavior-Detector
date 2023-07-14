5ddcb9fcea2f4fabb48d66d043c54557d7e681bc
Physics-Lee <hyperdunk2019@mail.ustc.edu.cn>
2023-07-14 09:08:07 +0800

    handle the short runs in the first or end of machine_label_v2.csv

--- function/integrate_into_reorientation_and_eliminate_short_reversal.m
+++ function/integrate_into_reorientation_and_eliminate_short_reversal.m
@@ -1,10 +1,15 @@
 function integrate_into_reorientation_and_eliminate_short_reversal(label_rearranged)
 
-% integrate turn and reversal into reorientation
+%% integrate turn and reversal into reorientation
 global s2frame
 mask = label_rearranged(:,3) == 1 | label_rearranged(:,3) == 3;
 label_rearranged(mask,3) = 200;
+
+% rearrange
 label_rearranged = re_rearrange_label(label_rearranged);
+
+%% eliminate runs shorter than threshold
+% label as reorientation
 t_threshold_run = 5; % s
 for i = 2:length(label_rearranged) - 1
     if label_rearranged(i,3) == 2 &&...
@@ -12,8 +17,19 @@ for i = 2:length(label_rearranged) - 1
         label_rearranged(i,3) = 200;
     end
 end
+
+% for the first and the last, label as unlabelled
+for i = [1,length(label_rearranged)]
+    if label_rearranged(i,3) == 2 &&...
+            label_rearranged(i,2) - label_rearranged(i,1) <= t_threshold_run*s2frame
+        label_rearranged(i,3) = 0;
+    end
+end
+
+% rearrange
 label_rearranged = re_rearrange_label(label_rearranged);
 
+%% save
 global folder_of_saved_files
 file_name = 'machine_label_v2.csv';
 output_label(label_rearranged, folder_of_saved_files, file_name)
