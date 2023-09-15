3f8cf08f19f32c45a519d510a7e4148baf47a2d3
Physics-Lee <hyperdunk2019@mail.ustc.edu.cn>
2023-09-15 17:13:15 +0800

    delete double check in round 2
    delete double check for the Euclidean distance of head and tail

--- main workflow/Tukey_test_of_distance_between_head_and_tail.m
+++ main workflow/Tukey_test_of_distance_between_head_and_tail.m
@@ -24,7 +24,7 @@ title('f(Euclidean distance between the head and the tail)');
 
 % perform Tukey test
 IQR_index = 3; % super parameter % bigger, stricter
-[~, ~, ~, mask_down,...
+[~, ~, mask_up, mask_down,...
     up_limit, down_limit, upper_bound, lower_bound] = ...
     Tukey_test(Euclid_distance_between_head_and_tail, IQR_index);
 
@@ -34,26 +34,5 @@ draw_lines(up_limit, down_limit, upper_bound, lower_bound);
 %% add label
 global label_number_outlier
 label(indices) = mask_down * 1 + mask_up * label_number_outlier;
-
-% %% label v2, to distinguish label of round 1 and round 2
-% label_v2 = label;
-% label_number = 11;
-% 
-% label_2_v2 = mask_down * label_number;
-% label_v2(indices) = label_2_v2;
-% label_rearranged_v2 = rearrange_label(label_v2);
-% 
-% %% double check
-% 
-% t_threshold_2 = 0.5; % s
-% 
-% is_passed_2 = double_check_for_Euclidean_distance_of_head_and_tail(...
-%     label_rearranged_v2,...
-%     label_number,...
-%     t_threshold_2);
-% 
-% if is_passed_2
-%     label(indices) = mask_down * 1; % if passed, add the result of round 2 to label.
-% end
 
 end
--- main workflow/maxContinuousFrames.m
+++
@@ -1,6 +0,0 @@
-function max_continuous_frames = maxContinuousFrames(label_rearranged, label_number)
-
-label_new = label_rearranged(label_rearranged(:,3) == label_number,:);
-max_continuous_frames = max(label_new(:,2) - label_new(:,1));
-
-end
