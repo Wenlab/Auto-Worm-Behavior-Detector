92de89da930cb9b8dfcf5ff1f3996485a50359aa
Yixuan Li <hyperdunk2019@mail.ustc.edu.cn>
2023-09-16 15:26:33 +0800

    use n_figs in output_figures.m

--- main_workflow/output_figures.m
+++ main_workflow/output_figures.m
@@ -1,19 +1,17 @@
 function output_figures(folder_of_saved_files)
 
-% Get the handles of all open figures
-figHandles = findall(0, 'Type', 'figure');
-
 % Loop through each figure and save it
-for i = numel(figHandles) - 1:-1:1 % the app will be recognized as a fig, so from end to start
+n_figs = 3;
+for i = 1:n_figs
     
     % Generate a unique file name for each figure
-    fileName = sprintf('figure%d.png', numel(figHandles) - i);
+    file_name = sprintf('figure_%d.png', i);
     
     % Set the current figure
-    figure(figHandles(i));
+    figure(i);
     
     % Save the figure
-    saveas(figHandles(i), fullfile(folder_of_saved_files, fileName));
+    saveas(figure(i), fullfile(folder_of_saved_files, file_name));
     
 end
 
