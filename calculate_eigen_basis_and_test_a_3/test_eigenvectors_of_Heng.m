c95ff1929d06b3d7b42d18ae6b292623017ca188
Physics-Lee <hyperdunk2019@mail.ustc.edu.cn>
2024-04-16 15:22:36 +0800

    Update test_eigenvectors_of_Heng.m

--- calculate_eigen_basis_and_test_a_3/test_eigenvectors_of_Heng.m
+++ calculate_eigen_basis_and_test_a_3/test_eigenvectors_of_Heng.m
@@ -1,8 +1,13 @@
-% load eigen_basis
-load('eigen_basis_with_turn.mat');
+%% load
 
 % from Heng
+load('eigen_basis_with_turn.mat');
 eigenvectors = EigenWorms;
+
+% from Yixuan
+eigenvectors = load_data_from_csv('eigen_basis_without_turn.csv');
+
+%% plot
 
 % plot first 4
 plot_first_4_coeff(eigenvectors);
@@ -13,3 +18,7 @@ radius = sqrt(eigenvectors(:, 1).^2 + eigenvectors(:, 2).^2);
 plot(radius);
 xlabel('segment');
 ylabel('sqrt(a_1^2 + a_2^2)');
+
+%% dot
+inner_product = eigenvectors' * eigenvectors;
+inner_product(abs(inner_product) < 1e-3) = 0;
